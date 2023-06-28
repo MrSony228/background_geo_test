2027101203db5c3bd2f422db965bf8edd47367a7
Dmitriy Ignatenkov <dmitriy.ignatenkov@ediweb.com>
2023-06-28 13:10:19 +0300

    Initial commit

---
+++ android/app/src/main/java/com/example/background_geo_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.background_geo_test;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterFragmentActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+    }
+}
